e09d3371e876c7037f15974da8142578c32454d3
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-02-16 14:44:43 +0200

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,18 +18,10 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+      Contact me:
+      </p>
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
