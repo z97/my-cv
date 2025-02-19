bcd7a5862c608289c5002db1c74fca837d8d66b6
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-02-19 20:03:12 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Vod.qa
+        ArtemPM.com
       </h1>
       <p className="mb-4">
         {`Hi, my name is Artem Klibaner. I can develop a website that will attract you more clients. I have extensive experience in product management, customer development and marketing. My projects:`}
