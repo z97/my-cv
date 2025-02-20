e2512b5d67ff2d9f1206312edfb7d77c2f96b861
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-02-20 09:10:33 +0200

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        {new Date().getFullYear()} Vod.qa
+        {new Date().getFullYear()} ArtemPM.com
       </p>
     </footer>
   )
