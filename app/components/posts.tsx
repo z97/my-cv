f37359f6d833371fcbf2ed591f40c348dc2e6536
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-02-21 15:57:29 +0200

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,7 +21,7 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div className="w-full flex flex-col space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400">
                 {post.metadata.title}
               </p>
