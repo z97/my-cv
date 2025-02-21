1432d28b47da9e6cde73cca1a9aa8b9b76c14a0e
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-02-21 16:01:04 +0200

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,7 +18,7 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-5"
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col space-x-0 md:space-x-2">
