a00b32cf331da1a5a5d785065f1fea30d1ae10ae
Artem Klibaner <32778606+z97@users.noreply.github.com>
2025-03-06 08:34:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         ArtemPM.com
       </h1>
       <p className="mb-4">
-        {`Hi, my name is Artem Klibaner. What attracts me to the work of a product manager? That a good product manager must be an attractive and charming person in life. It is very difficult to build a good product without improving yourself. I can develop a website that will attract you more clients. I have extensive experience in product management, customer development and marketing. My projects:`}
+        {`Hi, my name is Artem Klibaner. What attracts me to the work of a PM? That a good PM must be an attractive and charming person in life. It is very difficult to build a good product without improving yourself. I can develop a website that will attract you more clients. I have extensive experience in product management, customer development and marketing. My projects:`}
       </p>
       <div className="my-8">
         <BlogPosts />
